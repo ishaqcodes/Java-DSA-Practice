b0ab0a70dbd3cbe9e78d1f7a0fe19163f35385dd
ishaqcodes <shaikhmuhammedishaq@gmail.com>
2025-06-28 18:57:58 +0530

    BasicPatterns folder

---
+++ BasicPatterns/Character.java
@@ -0,0 +1,12 @@
+public class Character {
+    public static void main(String[] args) {
+        char character = 'A';
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(character + " ");
+                character++;
+            }
+            System.out.println();
+        }
+    }
+}
