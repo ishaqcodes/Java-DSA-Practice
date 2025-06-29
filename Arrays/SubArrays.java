bc29ed3d31cb5d0d7935101e31697b1356482736
ishaqcodes <shaikhmuhammedishaq@gmail.com>
2025-06-29 07:56:57 +0530

    updated SubArrays

--- Arrays/SubArrays.java
+++ Arrays/SubArrays.java
@@ -1,18 +1,24 @@
 public class SubArrays {
     public static void main(String[] args) {
-        int array[] = {2,4,6,8,10};
+        int array[] = {2, 4, 6, 8, 10};
         printSubArray(array);
     }
 
-    public static void printSubArray(int arr[]){
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length; j++){
-                for(int k = i; k <= j; k++){
+    public static void printSubArray(int arr[]) {
+        int totalSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {  // j starts from i
+                int currSum = 0;
+                for (int k = i; k <= j; k++) {
                     System.out.print(arr[k] + "  ");
+                    currSum += arr[k];
                 }
+                totalSum++;
+                System.out.println("Sum : " + currSum);
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total subArrays : " + totalSum);
     }
 }
